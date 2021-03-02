c9214e697d63930d46f680ab1946a35c3d24219c
thyagaraj9356 <79888379+thyagaraj9356@users.noreply.github.com>
2021-03-02 08:57:20 +0530

    Add files via upload

---
+++ Zoofoo.java
@@ -0,0 +1,25 @@
+class Zoofoo 
+{
+public static void main(String[] args)
+{  int i;
+  for(i=1;i<=10;i++)
+  {
+  if(i%2==0 && i%5==0)
+  {
+  System.out.println("ZooFoo");  
+  }
+  else if(i%2==0)
+  {
+  System.out.println("Zoo");
+  }
+  else if(i%5==0)
+  {
+  System.out.println("Foo");  
+  }
+  else
+  {
+  System.out.println(i);
+  }
+  }
+  }
+  }
